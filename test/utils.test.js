e26a3243844cc575dc087d12b3f8d01a64bfd645
leocavalcante <lc@leocavalcante.com>
2016-03-06 15:43:24 -0300

    Add and cover all keyNames possibilities

--- test/utils.test.js
+++ test/utils.test.js
@@ -22,14 +22,37 @@ describe('isInput', () => {
 
 describe('keyValues', () => {
   it('returns the key value of the given key name', () => {
+    expect(keyValues('ctrl')).to.be.equal('Control');
+    expect(keyValues('control')).to.be.equal('Control');
+    expect(keyValues('alt')).to.be.equal('Alt');
+    expect(keyValues('option')).to.be.equal('Option');
+    expect(keyValues('shift')).to.be.equal('Shift');
+    expect(keyValues('windows')).to.be.equal('Meta');
+    expect(keyValues('command')).to.be.equal('Meta');
+    expect(keyValues('esc')).to.be.equal('Escape');
+    expect(keyValues('escape')).to.be.equal('Escape');
+    expect(keyValues('backspace')).to.be.equal('Backspace');
+    expect(keyValues('tab')).to.be.equal('Tab');
+    expect(keyValues('enter')).to.be.equal('Enter');
+    expect(keyValues('return')).to.be.equal('Enter');
+    expect(keyValues('space')).to.be.equal(' ');
+    expect(keyValues('pause')).to.be.equal('Pause');
+    expect(keyValues('insert')).to.be.equal('Insert');
+    expect(keyValues('delete')).to.be.equal('Delete');
+    expect(keyValues('home')).to.be.equal('Home');
+    expect(keyValues('end')).to.be.equal('End');
+    expect(keyValues('pageup')).to.be.equal('PageUp');
+    expect(keyValues('pagedown')).to.be.equal('PageDown');
     expect(keyValues('left')).to.be.equal('ArrowLeft');
     expect(keyValues('up')).to.be.equal('ArrowUp');
     expect(keyValues('right')).to.be.equal('ArrowRight');
     expect(keyValues('down')).to.be.equal('ArrowDown');
-
+    expect(keyValues('capslock')).to.be.equal('CapsLock');
+    expect(keyValues('numlock')).to.be.equal('NumLock');
+    expect(keyValues('scrolllock')).to.be.equal('ScrollLock');
   });
 
   it('falls back to the key name if there is no key value for given key name', () => {
-    expect(keyValues('numlock')).to.be.equal('numlock');
+    expect(keyValues('break')).to.be.equal('break');
   });
 });
